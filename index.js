f72e312e5a75d1784455fcb5379408d7ca3b2e5d
SatomiJin <satomikoutarou0904Ggmail.com>
2024-09-10 16:05:09 +0700

    javascript day 23 sticky bar #23

--- index.js
+++ index.js
@@ -1,37 +1,15 @@
-const msg = new SpeechSynthesisUtterance();
-let voices = [];
-const voiceDropdown = document.querySelector(`[name=voice]`);
-const options = document.querySelectorAll(`[type=range],[name="text"]`);
-const speakButton = document.querySelector("#speak");
-const stopButton = document.querySelector("#stop");
-msg.text = document.querySelector(`[name="text"]`).value;
+const nav = document.querySelector(".nav-menu");
+const topOfNav = nav.offsetTop;
 
-const populateVoices = (e) => {
-  voices = e.target.getVoices();
-  voiceDropdown.innerHTML = voices
-    .map((voice) => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
-    .join("");
-};
-
-const setVoice = (e) => {
-  msg.voice = voices.find((voice) => voice.name === e.target.value);
-};
-
-const toggle = (stackOver = true) => {
-  speechSynthesis.cancel();
-  if (stackOver) {
-    speechSynthesis.speak(msg);
+const fixNav = (e) => {
+  //   console.log(topOfNav, window.scrollY);
+  if (window.scrollY >= topOfNav) {
+    document.body.style.paddingTop = nav.offsetHeight + "px";
+    document.body.classList.add("fixed-nav");
+  } else {
+    document.body.style.paddingTop = 0;
+    document.body.classList.remove("fixed-nav");
   }
 };
-const setOption = (e) => {
-  msg[e.target.name] = e.target.value;
-};
-const stopVoice = () => {
-  speechSynthesis.cancel();
-};
-speechSynthesis.addEventListener("voiceschanged", populateVoices);
-voiceDropdown.addEventListener("change", setVoice);
-options.forEach((item) => item.addEventListener("change", setOption));
-speakButton.addEventListener("click", toggle);
 
-stopButton.addEventListener("click", stopVoice);
+window.addEventListener("scroll", fixNav);
